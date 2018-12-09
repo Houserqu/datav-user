7d6b77b043b4829fcebed4f81306965c9e16b920
houser <houserqu@qq.com>
2018-12-09 15:31:19 +0800

    feat: 添加统一请求返回方法和参数校验方法

--- app/controller/user.js
+++ app/controller/user.js
@@ -33,14 +33,16 @@ class UserController extends Controller {
     const ctx = this.ctx;
     // 校验 `ctx.request.body` 是否符合我们预期的格式
     // 如果参数校验未通过，将会抛出一个 status = 422 的异常
-    ctx.validate(loginRule, ctx.request.body);
+    if (!ctx.helper.validate(loginRule)) return;
     // 调用 service 创建一个 topic
     const userInfo = await ctx.service.user.login(ctx.request.body);
     // 设置响应体和状态码
-    ctx.body = userInfo;
-    ctx.status = userInfo ? 200 : 404;
+    if (userInfo) {
+      ctx.helper.resSuccess(userInfo);
+    } else {
+      ctx.helper.resError('账号或密码错误');
+    }
   }
-
 }
 
 module.exports = UserController;
