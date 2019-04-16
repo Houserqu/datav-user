c0b9bd87b92faf6ae088986f5d36bbca1d09b56c
houser <houserqu@qq.com>
2019-04-16 11:48:39 +0800

    feat: check login, register

--- app/controller/user.js
+++ app/controller/user.js
@@ -1,83 +1,93 @@
-'use strict';
+"use strict"
 
-const Controller = require('egg').Controller;
-const fetch = require('node-fetch');
+const Controller = require("egg").Controller
+const fetch = require("node-fetch")
 
 // 定义创建接口的请求参数规则
 const createRule = {
-  mail: 'string',
-  nickname: 'string',
-  password: 'string',
-};
+  mail: "string",
+  nickname: "string",
+  password: "string"
+}
 
 const loginRule = {
-  mail: 'string',
-  password: 'string',
-};
+  mail: "string",
+  password: "string"
+}
 
 class UserController extends Controller {
   async create() {
-    const ctx = this.ctx;
-    ctx.validate(createRule, ctx.request.body);
+    const ctx = this.ctx
+    if (!ctx.helper.validate(createRule, ctx.request.body)) return
 
-    const id = await ctx.service.user.create(ctx.request.body);
+    try {
+      await ctx.service.user.create(ctx.request.body)
+    } catch (error) {
+      ctx.helper.resError("账号已存在")
+      return
+    }
     // 设置响应体和状态码
-    ctx.body = {
-      id,
-    };
-    ctx.status = 201;
+    ctx.helper.resSuccess("注册成功!")
   }
 
   async login() {
-    const ctx = this.ctx;
+    const ctx = this.ctx
     // 校验 `ctx.request.body` 是否符合我们预期的格式
     // 如果参数校验未通过，将会抛出一个 status = 422 的异常
-    if (!ctx.helper.validate(loginRule)) return;
+    if (!ctx.helper.validate(loginRule)) return
     // 调用 service 创建一个 topic
-    const userInfo = await ctx.service.user.login(ctx.request.body);
+    const userInfo = await ctx.service.user.login(ctx.request.body)
     // 设置响应体和状态码
     if (userInfo) {
-      ctx.helper.resSuccess(userInfo);
+      ctx.helper.resSuccess(userInfo)
     } else {
-      ctx.helper.resError('账号或密码错误');
+      ctx.helper.resError("账号或密码错误")
     }
   }
 
   async curUserInfo() {
-    const { ctx: { helper, service, headers, app: { config } } } = this;
-    const userId = headers['certificate-userid'];
+    const {
+      ctx: {
+        helper,
+        service,
+        headers,
+        app: { config }
+      }
+    } = this
+    const userId = headers["certificate-userid"]
 
     try {
-      const res = await service.user.getUser(userId);
-      const authRes = await fetch(`${config.AUTH_API_DOMAIN}/api/auth/user/${userId}`);
-      const result = await authRes.json();
-      console.log(result);
-
+      const res = await service.user.getUser(userId)
+      const authRes = await fetch(
+        `${config.AUTH_API_DOMAIN}/api/auth/user/${userId}`
+      )
+      const result = await authRes.json()
+      console.log(result)
+      const { password, ...rest } = res
       helper.resSuccess({
-        userInfo: {
-          mail: res.mail,
-          nickname: res.nickname,
-        },
+        userInfo: rest,
         permission: result.data
-      });
+      })
     } catch (e) {
-      helper.resError(e.message);
+      helper.resError(e.message)
     }
   }
 
   // 单个资源
   async show() {
-    const { ctx: { helper, service, params } } = this;
+    const {
+      ctx: { helper, service, params }
+    } = this
     try {
-      const res = await service.user.getUser(parseInt(params.id));
+      const res = await service.user.getUser(parseInt(params.id))
       helper.resSuccess({
         mail: res.mail,
-        nickname: res.nickname,
-      });
+        nickname: res.nickname
+      })
     } catch (e) {
-      helper.resError(e.message);
+      helper.resError(e.message)
     }
   }
 }
 
-module.exports = UserController;
+module.exports = UserController
--- app/router.js
+++ app/router.js
@@ -1,15 +1,16 @@
-'use strict';
+"use strict"
 
 /**
  * @param {Egg.Application} app - egg application
  */
 module.exports = app => {
-  const { controller } = app;
+  const { controller } = app
 
-  app.router.get('/api/user/my-info', controller.user.curUserInfo);
+  app.router.get("/api/user/my-info", controller.user.curUserInfo)
 
-  app.router.resources('user', '/api/user', controller.user);
+  app.router.post("/api/user/register", controller.user.create)
 
-  app.router.post('/api/login', controller.user.login);
+  app.router.post("/api/login", controller.user.login)
 
-};
+  app.router.resources("user", "/api/user", controller.user)
+}
